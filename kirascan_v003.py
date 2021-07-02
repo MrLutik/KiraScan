ee35ffe3ee20f4fb582604946d1095cda8eeaf9c
MrLutik <eugene@marine-trading.com>
2021-07-02 13:05:00 +0300

    new attempt

--- kirascan_v003.py
+++ kirascan_v003.py
@@ -3,9 +3,13 @@ from sys import stdout
 import requests
 from subprocess import Popen, PIPE, STDOUT
 
-class LinkChecker:
 
-    links_to_check = [
+class Node:
+    '''Collect all data from node and dump it to json'''
+    def __init__(self,ip) -> None:
+        self.node_ip = ip
+
+    check_links = [
                     ":16657/status", # SEED node
                     ":26657/status", # Sentry port
                     ":36657/status", # Priv_sentry port
@@ -13,20 +17,41 @@ class LinkChecker:
                     ":11000/api/kira/status",
                     ":11000/api/status",
                     ":11000/download/peers.txt",
-                    #":11000/download/snapshot.zip",
                     ]
 
-    def check(self, ip:str):
-        for link in LinkChecker.links_to_check:
+    def node_status(self) -> dict:
+        # Check status of given links. Add them to dict. 
+        # TODO: dump it to json
+        node_status ={"ip":f"{self.node_ip}"}
+        status={}
+        for link in Node.check_links:
             try:
-                req = requests.get(f"http://{ip}{link}")
+                req = requests.get(f"http://{self.node_ip}{link}")
+                print(f"http://{self.node_ip}{link}")
+                print(link, req.status_code)
             except Exception:
                 req = None
             if req:
-                print(req.status_code, link)
+                #print(req.__dict__)
+                d={f"{link}":f"{req.status_code}"}
+                status.update(d)
             else:
-                print(req, link)
+                d={f"{link}":"FAILED"}
+                status.update(d)
+        try:
+            req = requests.head(f"http://{self.node_ip}:11000/download/snapshot.zip")
+            print(f"http://{self.node_ip}:11000/download/snapshot.zip")
+        except Exception:
+            status.update({":11000/download/snapshot.zip":"FAILED"})
+        status.update({":11000/download/snapshot.zip":f"{req.status_code}"})
+        node_status["node_status"] = status
+        return node_status
+    
+    def node_info()->dict:
+        pass
 
-a = LinkChecker()
-a.check("144.91.74.43")
-#print(status1)
+if __name__ == '__main__':
+    a = Node("213.136.81.248")
+    print(a.node_status())
+    #print(dic)
+    #print(status1)
