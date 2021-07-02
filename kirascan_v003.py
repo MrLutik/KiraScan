f46976f66ad9366e3947224c727599021cfda3f1
MrLutik <yaievgeniy@gmail.com>
2021-07-02 19:34:53 +0300

    subprocess/multiprocessing version. Good perfomance

--- kirascan_v003.py
+++ kirascan_v003.py
@@ -1,4 +1,5 @@
-import requests
+import subprocess
+import multiprocessing as mp
 import json
 
 
@@ -6,8 +7,7 @@ class Node:
     '''Collect all data from node and dump it to json'''
     def __init__(self,ip) -> None:
         self.node_ip = ip
-        self.status = dict()
-        self.info = dict()
+        self.cpu = mp.cpu_count()
 
     check_links = [
                     ":16657/status", # SEED node
@@ -17,50 +17,27 @@ class Node:
                     ":11000/api/kira/status",
                     ":11000/api/status",
                     ":11000/download/peers.txt",
+                    ":11000/download/snapshot.zip"
                     ]
 
-    def node_status(self) -> dict:
+    def node_status(self,link) -> dict:
         # Check status of given links. Add them to dict. 
         # TODO: dump it to json
-        node_status ={"ip":f"{self.node_ip}"}
-        status={}
-        for link in Node.check_links:
-            try:
-                req = requests.get(f"http://{self.node_ip}{link}")
-                print(link, req.status_code)
-            except Exception:
-                req = None
-            if req:
-                #print(req.__dict__)
-                d={f"{link}":f"{req.status_code}"}
-                status.update(d)
+
+        with subprocess.Popen(['curl', '-r0-0', '--fail','-sS', f'http://{self.node_ip}{link}'],stderr=subprocess.PIPE,stdout=subprocess.PIPE) as proc:
+            resp = proc.stderr.read().decode('utf-8')
+            if resp != '':
+                print(link, 'FAILED')
             else:
-                d={f"{link}":"FAILED"}
-                status.update(d)
-        try:
-            req = requests.head(f"http://{self.node_ip}:11000/download/snapshot.zip")
-        except Exception:
-            status.update({":11000/download/snapshot.zip":"FAILED"})
-        status.update({":11000/download/snapshot.zip":f"{req.status_code}"})
-        node_status["node_status"] = status
-        self.status.update(node_status)
-        return node_status
-    
-    def node_info(self)->dict:
-        try:
-            req = requests.get(f"http://{self.node_ip}:56657/status")
-            print(":56657/status", req.status_code)
-        except Exception:
-            req = None
-        if req:
-            resp = json.loads(req.text)
-            proposer = resp["result"]["validator_info"]["address"]
-            print(proposer)
+                print(link, 'SUCCESS')
  
 
 if __name__ == '__main__':
     a = Node("213.136.81.248")
-    print(a.node_status())
-    a.node_info()
+    with mp.Pool(a.cpu) as pool:
+        res = pool.map_async(a.node_status, a.check_links)
+        res.get()
+
+    #a.node_info()
     #print(dic)
     #print(status1)
